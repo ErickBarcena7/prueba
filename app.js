47eb93f1a126af8ee4ba0b0be1ab44cddfb24777
erick <erickbarcena00@hotmail.com>
2020-05-02 05:02:37 -0500

    correcto

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-function getBom(){
-console.log(window.innerWidth);
-console.log(window.innerHeight);
-}
-getBom();
+window.addEventListener("load", function () {
+  var boton = document.getElementById("enviar");
+  var names = document.getElementById("name");
+  boton.addEventListener("click", function () {
+    var nombre = document.querySelector("#nombre").value;
+    var apellidos = document.querySelector("#apellidos").value;
+    if (nombre.trim() == null || nombre.trim().length <= 5 || !isNaN(nombre)) {
+      alert("El nombre no es válido");
+      names.innerHTML("n");
+      return false;
+    }
+    if (
+      apellidos.trim() == null ||
+      apellidos.trim().length == 0 ||
+      !isNaN(apellidos)
+    ) {
+      alert("Los apellidos no son válidos");
+      return false;
+    }
+  });
+});
